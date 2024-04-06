4c1b25906912ea33e095f9f639bfc48f9e55fe99
jewlmils <st.jewelmilagrosa@gmail.com>
2024-04-06 23:35:53 +0800

    feat: ensure traders are directed to the trader path only upon account approval

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,6 @@
 class PagesController < ApplicationController
   before_action :redirect_signed_in_user, only: :landing
-  before_action :require_trader, only: :trader
+  before_action :require_approved_trader, only: :trader
 
   def landing
   end
@@ -13,12 +13,12 @@ class PagesController < ApplicationController
   def redirect_signed_in_user
     if admin_signed_in?
       redirect_to admin_pages_path
-    elsif trader_signed_in?
-      redirect_to pages_trader_path
+    elsif current_trader && current_trader.approved?
+      redirect_to trader_path
     end
   end
 
-  def require_trader
-    redirect_to admin_pages_path, alert: "The page you're trying to access is for traders only." unless trader_signed_in?
+  def require_approved_trader
+    redirect_to root_path, alert: "Only approved traders can access this page." unless current_trader.approved?
   end
 end
