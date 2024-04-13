2507f324d0fe4d90a3419c550f7880252473eb0a
jewlmils <st.jewelmilagrosa@gmail.com>
2024-04-13 23:59:33 +0800

    refactor: clean up routes by removing redundant and unnecessary codes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,13 @@
 Rails.application.routes.draw do
-  
-  # resources :stocks do
-  #   collection do
-  #     post :buy  # Route for the 'buy' action
-  #   end
-  # end
+  root 'pages#landing'
+
   resources :stocks, only: [:index, :show] do
     post 'transactions/buy', to: 'transactions#buy', on: :member
   end
   
-
-  root 'pages#landing'
-  
   get 'admin/pending_traders', to: 'admin_pages#pending_traders'
   resources :admin_pages, path: 'admin', as: 'admin_pages'
   get 'trader/trader_dashboard', to: 'trader_pages#show'
-  # get 'trader/thankyou', to: 'pages#thankyou'
 
   devise_for :admins,  controllers: {
     sessions: 'admins/sessions',
@@ -27,4 +19,9 @@ Rails.application.routes.draw do
      registrations: 'traders/registrations'
    }
    
+  # resources :stocks do
+  #   collection do
+  #     post :buy  # Route for the 'buy' action
+  #   end
+  # end
 end
