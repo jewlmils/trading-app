77312373ef108077fe6ee0c8c658ca4a009940e0
dhanniela <tjmaureadhanniela@gmail.com>
2024-04-17 23:33:07 +0800

    feat: enable trader to deposit money to increase wallet balance

--- config/routes.rb
+++ config/routes.rb
@@ -17,6 +17,8 @@ Rails.application.routes.draw do
   get 'admin/pending_traders', to: 'admin_pages#pending_traders'
   resources :admin_pages, path: 'admin', as: 'admin_pages'
   get 'trader/trader_dashboard', to: 'trader_pages#show'
+  post 'trader/trader_dashboard', to: 'trader_pages#deposit'
+
 
   devise_for :admins,  controllers: {
     sessions: 'admins/sessions',
