9065e4f125a6ec8e7caa65a2e140b1dcb2155868
dhanniela <tjmaureadhanniela@gmail.com>
2024-04-17 21:42:11 +0800

    refactor: portfolios controller

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,20 +3,28 @@ class PortfoliosController < ApplicationController
   before_action :set_client
 
   def index
-    @trader = current_trader
-    @portfolios = @trader.portfolios
+    @portfolios = current_trader.portfolios
     @total_portfolio_value = calculate_total_portfolio_value(@portfolios)
-    @cash_balance = @trader.wallet
+    @cash_balance = current_trader.wallet
+    @quotes = {}
+
+    @portfolios.each do |portfolio|
+      portfolio.stocks.each do |stock|
+        @quotes[stock.ticker_symbol] = @client.quote(stock.ticker_symbol)
+      end
+    end
+    
     fetch_realtime_data_for_stocks(@portfolios)
   end
 
   def show
     @portfolio = current_trader.portfolios.find(params[:id])
     @stocks = @portfolio.stocks
-    @total_portfolio_value = calculate_single_stock_portfolio_value(@portfolio)
+    @single_stock_portfolio_value = calculate_single_stock_portfolio_value(@portfolio)
     @transactions = @portfolio.transactions.order(created_at: :desc)
     @performance_metrics = calculate_performance_metrics(@portfolio)
     @asset_allocation = calculate_asset_allocation(@portfolio)
+    
     fetch_realtime_data_for_stocks([@portfolio])
   end
 
@@ -58,14 +66,14 @@ class PortfoliosController < ApplicationController
   end
   
   def calculate_asset_allocation(portfolio)
-    total_portfolio_value = calculate_single_stock_portfolio_value(portfolio) # Calculate based on the current portfolio
+    total_portfolio_value = calculate_single_stock_portfolio_value(portfolio)
     asset_allocation = {}
   
     portfolio.stocks.each do |stock|
-      quote = @client.quote(stock.ticker_symbol)
-      latest_price = quote.latest_price
-      asset_allocation[stock.ticker_symbol] = (latest_price * portfolio.number_of_shares) / total_portfolio_value
+      allocation_percentage = (stock.price * portfolio.number_of_shares / total_portfolio_value) * 100
+      asset_allocation[stock.ticker_symbol] = allocation_percentage
     end
+  
     asset_allocation
   end
   
