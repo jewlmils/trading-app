5999b991b2428806fc636d9aeee24b78d7e3fad0
dhanniela <tjmaureadhanniela@gmail.com>
2024-04-21 20:25:04 +0800

    feat: implement daily update of stock prices, including days without transactions

--- app/models/stock_price.rb
+++ app/models/stock_price.rb
@@ -1,3 +1,13 @@
 class StockPrice < ApplicationRecord
   belongs_to :stock
+
+  def self.update_stock_prices_for_today(client)
+    stocks = Stock.all
+
+    stocks.each do |stock|
+      stock_price = StockPrice.find_or_create_by(stock_id: stock.id, date: Date.today)
+      quote = client.quote(stock.ticker_symbol)
+      stock_price.update(current_price: quote.latest_price)
+    end
+  end
 end
