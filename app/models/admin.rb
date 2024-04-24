af6a190fc434ea9e7ba93380caf42487ee887beb
jewlmils <st.jewelmilagrosa@gmail.com>
2024-04-24 16:32:28 +0800

    chore: install factory bot gem

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -2,4 +2,6 @@ class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable, :recoverable, :registerable, :rememberable, :validatable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
+
+  validates :email, presence: true
 end
