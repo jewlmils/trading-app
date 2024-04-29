6fdc86bceabf9d8a6c7a4b349001f78c22405df0
dhanniela <tjmaureadhanniela@gmail.com>
2024-04-29 11:05:07 +0800

    refactor: update cumulative_total_value_by_day method to use today's date as end date

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -18,23 +18,48 @@ class Portfolio < ActiveRecord::Base
     total_value
   end
 
-  def self.calculate_single_stock_portfolio_value(portfolio)
-    total_value = 0
+  # def self.calculate_single_stock_portfolio_value(portfolio)
+  #   total_value = 0
 
-    portfolio.stocks.each do |stock|
-      total_value = stock.price * portfolio.number_of_shares
-    end
+  #   portfolio.stocks.each do |stock|
+  #     total_value = stock.price * portfolio.number_of_shares
+  #   end
   
-    total_value
-  end
+  #   total_value
+  # end
   
+  # def self.cumulative_total_value_by_day
+  #   dates_with_transactions = Transaction.pluck(:created_at).map(&:to_date).uniq
+  
+  #   cumulative_values_by_day = {}
+  
+  #   dates_with_transactions.each do |date|
+  #     total_value = 0
+  #     transactions_up_to_date = Transaction.where("DATE(created_at) <= ?", date)
+  
+  #     transactions_up_to_date.each do |transaction|
+  #       stock_price = StockPrice.where(stock_id: transaction.stock_id, date: date).first
+  #       if transaction.transaction_type == 'buy'
+  #         total_value += transaction.number_of_shares * stock_price.current_price if stock_price
+  #       elsif transaction.transaction_type == 'sell'
+  #         total_value -= transaction.number_of_shares * stock_price.current_price if stock_price
+  #       end
+  #     end
+  
+  #     cumulative_values_by_day[date] = total_value
+  #   end
+  
+  #   cumulative_values_by_day
+  # end
+
   def self.cumulative_total_value_by_day
-    dates_with_transactions = Transaction.pluck(:created_at).map(&:to_date).uniq
+    end_date = Date.today
   
     cumulative_values_by_day = {}
   
-    dates_with_transactions.each do |date|
+    (Transaction.minimum(:created_at).to_date..end_date).each do |date|
       total_value = 0
+  
       transactions_up_to_date = Transaction.where("DATE(created_at) <= ?", date)
   
       transactions_up_to_date.each do |transaction|
@@ -51,4 +76,5 @@ class Portfolio < ActiveRecord::Base
   
     cumulative_values_by_day
   end
+  
 end
