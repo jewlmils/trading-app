7159bf952c7f26bf016151be8a0f363f7f2a8b69
dhanniela <tjmaureadhanniela@gmail.com>
2024-05-02 19:03:52 +0800

    feat: implement deposit amount validation and update input field styling

--- app/controllers/trader_pages_controller.rb
+++ app/controllers/trader_pages_controller.rb
@@ -3,21 +3,21 @@ class TraderPagesController < ApplicationController
     before_action :set_client
 
     def show
-        @trader = current_trader
-        @portfolios = @trader.portfolios
+        @portfolios = current_trader.portfolios
         @stocks = @portfolios.flat_map(&:stocks)
-        @transactions = @trader.transactions
+        @transactions = current_trader.transactions
     end
 
     def deposit
         deposit_amount = params[:amount].to_f
+        
         if deposit_amount <= 0
-          redirect_to root_path, alert: 'Deposit amount must be greater than zero.'
-          return
+            redirect_to root_path, alert: 'Please make sure the deposit amount is greater than zero.'
+            return
         end
     
         current_trader.update(wallet: current_trader.wallet + deposit_amount)
-        redirect_to root_path, notice: 'Deposit successful!'
+        redirect_to root_path, notice: 'Your deposit was successful. Happy trading!'
     end
   
   private
