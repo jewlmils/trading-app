7f7530ddc326ffe47a15055f30dfbfc3c63faa9d
dhanniela <tjmaureadhanniela@gmail.com>
2024-05-04 15:47:07 +0800

    fix migration to add missing 'unconfirmable_email' column

--- db/migrate/20240420064536_create_stock_prices.rb
+++ db/migrate/20240420064536_create_stock_prices.rb
@@ -2,9 +2,8 @@ class CreateStockPrices < ActiveRecord::Migration[7.1]
   def change
     create_table :stock_prices do |t|
       t.date :date
-      t.decimal :close_price
-      t.decimal :current_price
       t.references :stock, null: false, foreign_key: true
+      t.decimal :current_price
 
       t.timestamps
     end
