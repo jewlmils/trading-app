7c1a5b5f01f4395db129b418f94be348e4006b57
dhanniela <tjmaureadhanniela@gmail.com>
2024-05-04 11:32:53 +0800

    refactor: enhance transaction controller and model

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,129 +1,73 @@
 class TransactionsController < ApplicationController
-  before_action :require_trader
-  before_action :set_client                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
-
-  def index
-    @transactions = current_trader.transactions.order(created_at: :desc)
-    if params[:search].present?
-      @transactions = @transactions.joins(:stock).where("stocks.company_name ILIKE :search_start OR stocks.ticker_symbol ILIKE :search_start", search_start: "#{params[:search]}%")
-    end
-
-    @pagy, @paginated_transactions = pagy(@transactions, items: 5)
-    
-    @portfolio = current_trader.portfolios
-    @cash_balance = current_trader.wallet
-  end
-
-  def buy
-    @trader = current_trader
-    @symbol = params[:id]
-    @quote = @client.quote(@symbol)
-    @quantity = params[:quantity].to_i
-    @total_price = @quantity * @quote.latest_price
-
-    save_to_db(@symbol, @quote)
-
-    if @trader.wallet >= @total_price
-      execute_transaction(@trader, @symbol, @quantity, 'buy', @total_price)
-      flash[:notice] = 'Stock purchased successfully.'
-      redirect_to portfolios_path
-    else
-      flash[:alert] = 'Insufficient funds to buy the stock.'
-      redirect_to stock_path
-    end
-
-    rescue IEX::Errors::SymbolNotFoundError => e
-      handle_error
-  end
-
-  def sell
-    @trader = current_trader
-    @symbol = params[:id]
-    @quote = @client.quote(@symbol)
-    @quantity = params[:quantity].to_i
-    portfolio = @trader.portfolios.find_by(stock_id: Stock.find_by(ticker_symbol: @symbol)&.id)
-      if portfolio.nil? || portfolio.number_of_shares <= @quantity
-        flash[:alert] = "You don't have enough shares to sell."
-        redirect_to stock_path
-      end
-    @total_price = @quantity * @quote.latest_price
-    execute_transaction(@trader, @symbol, @quantity, 'sell', @total_price)
-    flash[:notice] = 'Stock sold successfully.'
-    redirect_to portfolios_path
-    
-    rescue IEX::Errors::SymbolNotFoundError => e
-      handle_error
-  end
+    before_action :require_trader
+    before_action :set_client                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                              
   
-  private
+    def index
+        @all_transactions = current_trader.transactions.order(created_at: :desc)
+        @transactions = Transaction.search(params[:search], @all_transactions)
 
-  def execute_transaction(trader, symbol, quantity, transaction_type, total_price)
-    ActiveRecord::Base.transaction do
-      if transaction_type == 'buy'
-        trader.update!(wallet: trader.wallet - total_price)
-      elsif transaction_type == 'sell'
-        trader.update!(wallet: trader.wallet + total_price)
-      end
-  
-      create_transaction(trader, symbol, quantity, transaction_type, total_price)
+        if @transactions.present?
+            @pagy, @paginated_transactions = pagy(@transactions, items: 5)
+        else
+            flash[:alert] = 'Looks like there are no transactions for that stock. Try again!'
+            redirect_to transactions_path
+        end
+        
+        @cash_balance = current_trader.wallet
     end
-  end
+  
+    def buy
+        set_transaction_data    
 
-  def create_transaction(trader, symbol, quantity, transaction_type, total_price)
-    stock = Stock.find_or_create_by(ticker_symbol: symbol) do |s|
-      s.company_name = @quote.company_name
-      s.price = @quote.latest_price
+        Stock.save_to_db(@symbol, @quote)
+    
+        if current_trader.wallet >= @total_price
+            Transaction.execute_transaction(@quote, current_trader, @symbol, @quantity, 'buy', @total_price)
+            flash[:notice] = 'Yay! Stock purchased successfully. It\'s gonna be a great investment!'
+            redirect_to portfolios_path
+        else
+            flash[:alert] = 'Oops! Insufficient funds to buy the stock. Top up your wallet!'
+            redirect_to stock_path
+        end
+    
+        rescue IEX::Errors::SymbolNotFoundError
+            handle_error
+    end
+  
+    def sell
+        set_transaction_data
+
+        portfolio = current_trader.portfolios.find_by(stock_id: Stock.find_by(ticker_symbol: @symbol)&.id)
+        
+        if portfolio.nil? || portfolio.number_of_shares <= @quantity
+            flash[:alert] = 'Looks like you\'re running low on shares. Time to stock up before selling!'
+            redirect_to stock_path
+        end
+        
+        Transaction.execute_transaction(@quote, current_trader, @symbol, @quantity, 'sell', @total_price)
+        flash[:notice] = 'Stock successfully sold! Keep the profits rolling in!'
+        redirect_to portfolios_path
+        
+        rescue IEX::Errors::SymbolNotFoundError
+            handle_error
     end
     
-    stock_price = StockPrice.find_or_create_by(stock_id: stock.id, date: Date.today)
-    stock_price.update(current_price: @quote.latest_price)
-    portfolio = trader.portfolios.find_or_create_by(stock_id: stock.id)
-    portfolio_stock = PortfolioStock.find_or_create_by(portfolio_id: portfolio.id, stock_id: stock.id)
+    private
 
-    if transaction_type == 'buy'
-      portfolio.update!(number_of_shares: portfolio.number_of_shares + quantity.to_i)
-    elsif transaction_type == 'sell'
-      portfolio.update!(number_of_shares: portfolio.number_of_shares - quantity.to_i)
+    def set_transaction_data
+        @symbol = params[:id]
+        @quote = @client.quote(@symbol)
+        @quantity = params[:quantity].to_i
+        @total_price = @quantity * @quote.latest_price
     end
   
-    Transaction.create!(
-      trader_id: trader.id,
-      stock_id: stock.id,
-      portfolio_id: portfolio.id,
-      transaction_type: transaction_type,
-      number_of_shares: quantity,
-      price: total_price
-    )
-  end
-  
-  def save_to_db(symbol, quote)  
-    existing_stock = Stock.find_by(ticker_symbol: @symbol)
-
-    if existing_stock
-      existing_stock.update!(
-        ticker_symbol: quote.symbol,
-        company_name: quote.company_name,
-        price: quote.latest_price
-      )
-    else
-      Stock.create!(
-        ticker_symbol: quote.symbol,
-        company_name: quote.company_name,
-        price: quote.latest_price
-      )
+    def handle_error
+        flash[:alert] = "Uh-oh! We couldn't find that symbol. Please check the symbol and try again."
+        redirect_to stocks_path
     end
-  end
 
-  def handle_error
-    flash[:alert] = 'Failed to retrieve stock information from the IEX API.'
-
-    redirect_to stocks_path
-  end
-
-  private
-
-  def require_trader
-    redirect_to root_path, alert: "You are not authorized to access this page." unless trader_signed_in?
-  end
-
+    def require_trader
+        redirect_to root_path, alert: "You are not authorized to access this page." unless trader_signed_in?
+    end
+  
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,22 @@
-class Stock < ActiveRecord::Base
+class Stock < ApplicationRecord
     has_many :portfolio_stocks
     has_many :portfolios, through: :portfolio_stocks
     has_many :transactions
     has_many :stock_prices
+
+    def self.save_to_db(symbol, quote)  
+        stock_attributes = {
+            ticker_symbol: quote.symbol,
+            company_name: quote.company_name,
+            price: quote.latest_price
+        }
+
+        existing_stock = Stock.find_by(ticker_symbol: symbol)
+    
+        if existing_stock
+            existing_stock.update!(stock_attributes)
+        else
+            Stock.create!(stock_attributes)
+        end
+    end
 end
